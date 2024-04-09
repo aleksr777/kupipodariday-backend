60675a5bdb53c7b113ed52e0716218d04bc5ae44
Aleksandr Petrov <petrov.aleksr@gmail.com>
2024-04-09 11:17:16 +0500

    feat: cгенерировал каркас API через Nest CLI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { UsersModule } from './users/users.module';
+import { WishesModule } from './wishes/wishes.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
+import { OffersModule } from './offers/offers.module';
 
 @Module({
   imports: [
@@ -14,6 +18,10 @@ import { AppController } from './app.controller';
       entities: [__dirname + '/**/*.entity{.ts,.js}'], // Путь к сущностям
       synchronize: true,
     }),
+    UsersModule,
+    WishesModule,
+    WishlistsModule,
+    OffersModule,
   ],
   controllers: [AppController],
   providers: [],
