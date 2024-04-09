5393d92fd449f9a44d60c9980b6d50ac80f3fc49
Aleksandr Petrov <petrov.aleksr@gmail.com>
2024-04-09 10:32:28 +0500

    feat: подключена база данных

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'student',
+      password: 'student',
+      database: 'nest_project',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // Путь к сущностям
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
