42a93dfcf5507d88c3ec05340c4c5b4b7d0d4fdf
Aleksandr Petrov <petrov.aleksr@gmail.com>
2024-04-17 22:48:52 +0500

    fix: изменён порт и правка подключения к базе данных

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,10 @@ import { UsersModule } from './users/users.module';
 import { WishesModule } from './wishes/wishes.module';
 import { WishlistsModule } from './wishlists/wishlists.module';
 import { OffersModule } from './offers/offers.module';
+import { User } from './users/entities/user.entity';
+import { Wish } from './wishes/entities/wish.entity';
+import { Offer } from './offers/entities/offer.entity';
+import { Wishlist } from './wishlists/entities/wishlist.entity';
 
 @Module({
   imports: [
@@ -14,8 +18,8 @@ import { OffersModule } from './offers/offers.module';
       port: 5432,
       username: 'student',
       password: 'student',
-      database: 'nest_project',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      database: 'kupipodariday',
+      entities: [Offer, User, Wish, Wishlist],
       synchronize: true,
     }),
     UsersModule,
